6f8af9378e019fac2218f67f6957af832fb6dd94
uttamsaha <uttamsaha982@gmail.com>
2022-05-13 21:22:39 +0600

    transaction added

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const sha256 = require('crypto-js/sha256'); //for creating hash
 --------------------------------------------------------------------------------------------------------------------------------------------*/
 //creating block
 class Block {
-    constructor(timestamp, data, previousHash = ""){ //previous has default value empty string
+    constructor(timestamp, transactions, previousHash = ""){ //previous has default value empty string
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
@@ -15,7 +15,7 @@ class Block {
 
 
     //MINING TO GET VALID HASH BLOCK
-    nineBlock(difficulty){
+    mineBlock(difficulty){
         //ei function er kaj jotokkhon porjonto valid block hash na pai totokkhon porjonto hash calculat kora
         while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")){
             //condition in white (this.hash.substring(0, difficulty) !=="00000") // jotokkhon hash er first 5 ta char 00000 na hoi totokkhon hash calcualte korbe. 00000.. diye start hash ei valid hash hisebe nibo
@@ -26,18 +26,28 @@ class Block {
     }
     //hashCreating function
     calculateHash() {
-        return sha256(this.timestamp + JSON.stringify(this.data )+ this.previousHash + this.nonce).toString(); // ensure making string all element && converting hash to string
+        return sha256(this.timestamp + JSON.stringify(this.transactions )+ this.previousHash + this.nonce).toString(); // ensure making string all element && converting hash to string
     }
 }
 
 /*--------------------------------------------------------------------------------------------------------------------------------------------
                                             ------ START OF BLOCKCHAIN CLASS -----
 --------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//transaction class
+ class Transaction {
+     constructor(fromAddress, toAddress, amount){
+         this.fromAddress = fromAddress;
+         this.toAddress = toAddress;
+         this.amount = amount;
+     }
+ }
 //crating block chain
 class Blockchain{
     constructor() {
         this.cain = [this.generateGenesisBlock()]; //empty array in which array every block will be inserted , chain e contain genenis block first
         this.difficulty = 5; //assuming difficulty 2
+        this.pendingTransactions = [];  //block ta jokn mining hobe tokn transaction hobe er age pending thakbe
     }
 
     //generate genesisBlock
@@ -50,12 +60,25 @@ class Blockchain{
         return this.cain[this.cain.length - 1]; //array index
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash //setting new blocks previous hash
-    //    newBlock.hash = newBlock.calculateHash();
-        newBlock.nineBlock(this.difficulty);
-        this.cain.push(newBlock);
+    //create transaction function
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
     }
+
+    //mine pending transaction
+    minePendingTransaction() {
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+        this.cain.push(block); //push after mine
+        this.pendingTransactions = []; //empty pending transaction after push 
+    }
+
+    // addBlock(newBlock) {
+    //     newBlock.previousHash = this.getLatestBlock().hash //setting new blocks previous hash
+    // //    newBlock.hash = newBlock.calculateHash();
+    //     newBlock.mineBlock(this.difficulty);
+    //     this.cain.push(newBlock);
+    // }
 
 /*-------------------------------------------------------------------------------------------------------------------------------------------
                                         --------- CHECKING THE BLOCK IS VALID OR NOT ----------
@@ -78,12 +101,8 @@ class Blockchain{
 }
 
 const josscoin = new Blockchain();
-const block = new Block('2019-01-01',{ amount: 5});
-const block2 = new Block('2019-01-01',{ amount: 10});
-// console.log(block);
+josscoin.createTransaction(new Transaction('address1','address2',100));
+josscoin.createTransaction(new Transaction('address2','address1',50)); //transaction gula process hobe jokhon block ta mine kora hobe.
 
-josscoin.addBlock(block); //block will be added into Blockchain
-// josscoin.addBlock(block2);
-
-josscoin.addBlock(block2);
+josscoin.minePendingTransaction();
 console.log(josscoin);
